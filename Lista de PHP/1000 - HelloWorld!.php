20d58bbec65b44668a291c69b053123112f686ba
vandevan <vandevanpassos123@gmail.com>
2023-07-19 15:54:27 -0300

    Primeiro

---
+++ Lista de PHP/1000 - HelloWorld!.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    
+    <?php echo '<p>Hello World</p>'; ?> 
+
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
