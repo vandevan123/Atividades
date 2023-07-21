ef8224ae488f60e0dfff98babffee34aed44d126
vandevan <vandevanpassos123@gmail.com>
2023-07-21 14:37:12 -0300

    Segundo

---
+++ Lista de PHP/1001 - Extremamente fácil.php
@@ -0,0 +1,11 @@
+<?php
+// Leitura dos valores inteiros
+$A = intval(readline());
+$B = intval(readline());
+
+// Efetuando a soma
+$X = $A + $B;
+
+// Imprimindo o resultado
+echo "X = " . $X . PHP_EOL;
+?>
